b3a8c7478219331cefedecffb125bb514ceed7fc
zxp <179603004@qq.com>
2020-07-14 22:26:28 +0800

    132123

---
+++ GdiDemo/GdiDemo/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace GdiDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myGaugePlate1 = new WindowsFormsControlPro.MyGaugePlate();
+            this.SuspendLayout();
+            // 
+            // myGaugePlate1
+            // 
+            this.myGaugePlate1.Circlecolor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(10)))), ((int)(((byte)(30)))));
+            this.myGaugePlate1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myGaugePlate1.Location = new System.Drawing.Point(0, 0);
+            this.myGaugePlate1.MyProperty = 0;
+            this.myGaugePlate1.Name = "myGaugePlate1";
+            this.myGaugePlate1.Pointcolor = System.Drawing.Color.SeaGreen;
+            this.myGaugePlate1.Size = new System.Drawing.Size(701, 450);
+            this.myGaugePlate1.TabIndex = 0;
+            this.myGaugePlate1.Textscale = 0.8F;
+            this.myGaugePlate1.Textshowscale = 0.85F;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 450);
+            this.Controls.Add(this.myGaugePlate1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WindowsFormsControlPro.MyGaugePlate myGaugePlate1;
+    }
+}
+
---
+++ GdiDemo/WindowsFormsControlPro/MyGaugePlate.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlPro
+{
+    public partial class MyGaugePlate : UserControl
+    {
+        public MyGaugePlate()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.DoubleBuffer, true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.SetStyle(ControlStyles.Selectable, true);
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+
+        }
+
+        #region 属性设置
+
+        private Color _pointcolor=Color.BlueViolet;
+
+        [Category("自定义属性")]
+        [Description("指针颜色")]
+        public Color Pointcolor
+        {
+            get { return _pointcolor; }
+            set { _pointcolor = value;
+
+                this.Invalidate();
+            }
+        }
+
+
+
+
+        private Color _circlecolor=Color.FromArgb(20,10,30);
+
+        [Category("自定义属性")]
+        [Description("外环颜色")]
+        public Color Circlecolor
+        {
+            get { return _circlecolor; }
+            set { _circlecolor = value;
+
+                this.Invalidate();
+              }
+        }
+
+        [Category("自定义属性")]
+        [Description("文本圆占的比例、设置要小于1.0")]
+
+        private float _textscale=0.8f;
+        public float Textscale
+        {
+            get { return _textscale; }
+            set {
+
+                if (value>1.0)
+                {
+                    return;
+                }
+                _textscale=value;
+
+                this.Invalidate();
+            }
+        }
+
+        [Category("属性设置")]
+        [Description("文本显示高度比例")]
+        private float _textshowscale=0.85f;
+
+        public float Textshowscale
+        {
+            get { return _textshowscale; }
+            set {
+
+                if (value>1.0)
+                {
+                    return;
+                }
+                _textshowscale=value;
+
+                this.Invalidate();
+            }
+        }
+
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+        #endregion
+
+        #region 渐变色设置
+
+        private Color GetStartLinearColor(Color EndLinearColor,float lineareScale)
+        {
+            return Color.FromArgb((int)(EndLinearColor.R-(EndLinearColor.R)* lineareScale), (int)(EndLinearColor.G - EndLinearColor.G * lineareScale), (int)(EndLinearColor.B - EndLinearColor.B * lineareScale));
+        }
+        #endregion
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            var g =e.Graphics;
+
+
+
+            //var pen = new Pen(Pointcolor, 5);
+
+            var pen = new Pen(GetStartLinearColor(Pointcolor, 0.3f), 5);
+
+            pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+
+           
+
+            g.DrawLine(pen, new Point(0, 0), new Point(this.Width/3, this.Height/3));
+
+             pen = new Pen(GetStartLinearColor(Pointcolor, 0.5f), 5);
+
+            g.DrawLine(pen, new Point(this.Width / 3, this.Height / 3), new Point(2*this.Width / 3, 2*this.Height / 3));
+
+             pen = new Pen(GetStartLinearColor(Pointcolor, 0.8f), 5);
+
+            g.DrawLine(pen, new Point(2 * this.Width / 3, 2 * this.Height / 3), new Point(this.Width, this.Height));
+
+
+        }
+    }
+}
